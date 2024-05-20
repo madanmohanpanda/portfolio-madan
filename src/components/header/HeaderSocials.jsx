c976b08a3af6d0baf3d09c80fc4ff6cfee0dd890
madan-akrabhi <116197719+madanmohanpanda@users.noreply.github.com>
2024-05-20 14:05:56 +0530

    update

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-        <a href="https://www.linkedin.com/in/madan-mohan-panda-511aa924a/" target='__blank'><FaLinkedin /></a>
-        <a href="https://leetcode.com/Madan8750/"><SiLeetcode /></a>
-        <a href="https://github.com/madanmohanpanda" target='__blank'><FaGithub /></a>
+      <a
+        href="https://www.linkedin.com/in/madan-mohan-panda-511aa924a/"
+        target="__blank"
+      >
+        <FaLinkedin />
+      </a>
+      <a href="https://leetcode.com/Madan8750/">
+        <SiLeetcode />
+      </a>
+      <a href="https://github.com/madanmohanpanda" target="__blank">
+        <FaGithub />
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default HeaderSocials;
